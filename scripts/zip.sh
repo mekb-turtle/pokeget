fa1a3404af1cede88b205f75b2e60219437f4f52
talwat <talrac1020@gmail.com>
2022-03-03 20:09:11 +0100

    Updated zip script to generate a .deb file

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -2,5 +2,33 @@
 
 # This script zips the project's source code into a tarball
 
-tar cvf pokeget.tar Makefile pokeget 
-tar cvf pokeget-lite.tar Makefile pokeget 
+rm -rf output
+mkdir output
+
+echo "Making source tarballs..."
+tar cvf output/pokeget-src.tar Makefile pokeget 
+tar cvf output/pokeget-src-lite.tar Makefile pokeget
+
+echo "Making .deb package..."
+rm -rf output/pokeget-deb
+mkdir output/pokeget-deb
+mkdir -p output/pokeget-deb/usr/bin
+cp pokeget output/pokeget-deb/usr/bin
+
+mkdir -p output/pokeget-deb/DEBIAN
+touch output/pokeget-deb/DEBIAN/control
+printf "Package: pokeget
+Version: 1.0
+Architecture: all
+Maintainer: Talwat
+Description: pokeget is a bash script you can use to display cool sprites of pokemon in your terminal
+" >> output/pokeget-deb/DEBIAN/control
+
+touch output/pokeget-deb/DEBIAN/postinst
+printf "#!/bin/sh
+chmod +x /usr/bin/pokeget
+exit 0
+" >> output/pokeget-deb/DEBIAN/postinst
+
+chmod -v 775 output/pokeget-deb/DEBIAN/postinst
+dpkg-deb --build --root-owner-group output/pokeget-deb
