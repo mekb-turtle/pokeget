fed65a00bbd6c5e8637622c0de2609799282561d
talwat <talrac1020@gmail.com>
2022-03-05 19:25:15 +0100

    Updated version to 1.1

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PREFIX = ~/.local/
-VERSION = 1.0
+VERSION = 1.1
 
 all:
 	@echo "pokeget version $(VERSION)"
--- scripts/dev/get_pokedex.sh
+++ scripts/dev/get_pokedex.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 curl -s 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number' \
         | tr -d '\n' \
