7aa24c603cf334aa44cfcccea9a2dd726cbb60bf
talwat <talrac1020@gmail.com>
2022-03-10 19:43:39 +0100

    Finalized homebrew support

--- metadata/pokeget.rb
+++ metadata/pokeget.rb
@@ -5,7 +5,7 @@ class Pokeget < Formula
   desc \"Bash script that displays cool sprites of pokemon in your terminal\"
   homepage \"https://github.com/talwat/pokeget\"
   url \"https://github.com/talwat/pokeget/releases/download/$VERSION/pokeget_$VERSION-src.tar\"
-  sha256 \"99ed742c4833cf922d65419c759b3ab5c149f12b1ca2b6967bde3fb2762bb5df\"
+  sha256 \"[insert checksum]\"
   license \"MIT\"
   head \"https://github.com/talwat/pokeget.git\", branch: \"main\"
 
